f9fe3953b5d53f81e57c4a8ce287951e58c965e4
Ben Murray <ben.murray@quantitech.com>
2022-02-17 17:23:08 -0600

    first working version

--- wordleSolver.py
+++ wordleSolver.py
@@ -4,6 +4,7 @@ import sys
 import signal
 import pandas as pd
 from qtpy import uic, QtCore, QtWidgets, QtGui
+from qtpy.QtGui import QPalette
 from os.path import join, dirname
 
 UI_AppWindow, WindowType = uic.loadUiType(join(dirname(__file__), "wordle_solver.ui"))
@@ -14,6 +15,10 @@ class WSMainWindow(QtWidgets.QMainWindow, UI_AppWindow):
         QtWidgets.QMainWindow.__init__(self, flags=QtCore.Qt.Window)
         UI_AppWindow.__init__(self)
         self.setupUi(self)
+
+        self.known_letters_with_pos = {}
+        self.known_letters_bad_pos = {}
+        self.known_bad_chars = []
 
         self.cell_state_options = ['white', 'grey', 'yellow', 'green']
 
@@ -46,6 +51,12 @@ class WSMainWindow(QtWidgets.QMainWindow, UI_AppWindow):
         self._setup_click_actions()
         self._setup_menubar()
         self.statusBar()
+        self.possible_words = self.get_all_possible_words()
+
+    def get_all_possible_words(self):
+        """ Reads a list of words sorted my most frequency in Holman Bible"""
+        words = pd.read_csv("words.csv")
+        return words['word'].values
 
     def return_pressed(self):
         lineedit = QtWidgets.QApplication.focusWidget()
@@ -64,9 +75,63 @@ class WSMainWindow(QtWidgets.QMainWindow, UI_AppWindow):
         self.actionQuit.triggered.connect(self.close)
 
     def _setup_click_actions(self):
+        # setup color changing of LineEdits
         for row in range(5):
             for col in range(5):
                 self.inputs[row][col].returnPressed.connect(self.return_pressed)
+
+        # setup Submit of row
+        self.row0Submit.clicked.connect(self.submit_clicked)
+        self.row1Submit.clicked.connect(self.submit_clicked)
+        self.row2Submit.clicked.connect(self.submit_clicked)
+        self.row3Submit.clicked.connect(self.submit_clicked)
+        self.row4Submit.clicked.connect(self.submit_clicked)
+
+    def submit_clicked(self):
+        button = QtWidgets.QApplication.focusWidget()
+        row = int(button.objectName()[3])
+        # Get letter and status of each input and add to appropriate array
+        for col in range(5):
+            line_edit = self.inputs[row][col]
+            char = line_edit.text()
+            color = self.get_color(line_edit.palette().color(QPalette.Background))
+
+            if color == "green":
+                if char in self.known_letters_with_pos.keys():  # Add it to its value
+                    self.known_letters_with_pos[char].append(col)
+                else:
+                    self.known_letters_with_pos[char] = [col]
+            if color == "grey":
+                if char not in self.known_bad_chars:
+                    self.known_bad_chars.append(char)
+            if color == "yellow":
+                if char in self.known_letters_bad_pos.keys():  # Add it to its value
+                    self.known_letters_bad_pos[char].append(col)
+                else:
+                    self.known_letters_bad_pos[char] = [col]
+
+        self.possible_words = get_words(self.possible_words,
+                                        self.known_bad_chars,
+                                        self.known_letters_bad_pos,
+                                        self.known_letters_with_pos)
+        self.try_next_text.setText(f"Try: {self.possible_words[0]}")
+        min_idx = min(len(self.possible_words), 4)
+        next_top_str = f"Next Top {min_idx - 1}:"
+        for idx in range(min_idx - 1):
+            next_top_str += f" {self.possible_words[idx+1]},"
+        self.top_results.setText(next_top_str[:-1])
+        #disable button, enable the next one
+
+    def get_color(self, bg):
+        #bg is background of of Cell
+        if bg.red() == 239 and bg.green() == 239 and bg.blue() == 239:
+            return "white"
+        if bg.red() == 128 and bg.green() == 128 and bg.blue() == 128:
+            return "grey"
+        if bg.red() == 255 and bg.green() == 255 and bg.blue() == 0:
+            return "yellow"
+        if bg.red() == 0 and bg.green() == 128 and bg.blue() == 0:
+            return "green"
 
 
 def get_words(possibles, bad_chars, known_letters_bad_pos, known_letters_with_pos):
